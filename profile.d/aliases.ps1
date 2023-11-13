f5141d38c9f485c9263165f9d567b1c22af29d2c
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-11-13 19:42:40 +0800

    feat(completions): `Invoke-Build`

--- profile.d/aliases.ps1
+++ profile.d/aliases.ps1
@@ -5,3 +5,4 @@ Set-Alias -Name condaaa -Value Start-Conda
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name rgaf -Value Search-RgaFzf
 Set-Alias -Name psf -Value Format-PowerShellScript
+Set-Alias -Name ib -Value Invoke-Build
