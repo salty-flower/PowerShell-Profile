6dfce3b6fae20e598570de79b7598483dede9ca4
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-12-06 14:09:09 +0800

    feat: add `invoke-build`

--- profile.d/modules.ps1
+++ profile.d/modules.ps1
@@ -1,3 +1,4 @@
 Import-Module "${SCOOP_HOME}\modules\scoop-completion"
 Import-Module "${SCOOP_HOME}\modules\lazy-posh-git"
 Import-Module -Name CompletionPredictor
+Import-Module "${SCOOP_HOME}\apps\invoke-build\current\InvokeBuild.psm1"
