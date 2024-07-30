df22cce19810253a09d7b2e1c372a3c441a11a02
salty-flower <95509524+salty-flower@users.noreply.github.com>
2024-07-30 09:30:04 +0800

    feat: add dotnet completion

---
+++ profile.d/completions.d/dotnet.ps1
@@ -0,0 +1,7 @@
+# From https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
